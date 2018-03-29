60c1a12a72432825ef6350fcee5a304a95ccd117
hifistar <yenshan@gk9.so-net.ne.jp>
2018-03-30 06:30:23 +0900

    revised, and add algorithm

--- prog_math_puzzle/repeat_combination.hs
+++ prog_math_puzzle/repeat_combination.hs
@@ -5,7 +5,8 @@ combination xs ys 0 = [ys]
 combination xs ys n = concat [combination xs (ys++[x]) (n-1) | x <- xs]
 
 repeat_combination :: [Int] -> Int -> [[Int]]
-repeat_combination xs n | n <= 1 = [[]]
-                        | otherwise = combination xs [] n
+repeat_combination xs n = combination xs [] (n+1)
+
+
 main :: IO ()
 main = do print $ repeat_combination [1,2,3] 3
---
+++ prog_math_puzzle/repeat_combination3.hs
@@ -0,0 +1,13 @@
+
+
+combination :: Int -> [[Int]] -> [[Int]]
+combination x ys = [ x : y | y <- ys ]
+
+repeat_combination :: [Int] -> Int -> [[Int]]
+repeat_combination xs 0 = [ [x] | x <- xs]
+repeat_combination xs n = concat [ combination x ys | x <- xs]
+                          where
+                            ys = repeat_combination xs (n-1)
+
+main :: IO ()
+main = do print $ repeat_combination [1,2,3] 3
