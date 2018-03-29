cc213544c47963af11ac13442af8522fd9c6062c
hifistar <yenshan@gk9.so-net.ne.jp>
2018-03-30 06:46:09 +0900

    revised implementation.

--- prog_math_puzzle/repeat_combination.hs
+++ prog_math_puzzle/repeat_combination.hs
@@ -5,7 +5,7 @@ combination xs ys 0 = [ys]
 combination xs ys n = concat [combination xs (ys++[x]) (n-1) | x <- xs]
 
 repeat_combination :: [Int] -> Int -> [[Int]]
-repeat_combination xs n = combination xs [] (n+1)
+repeat_combination xs n = combination xs [] n
 
 
 main :: IO ()
