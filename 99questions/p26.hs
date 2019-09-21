2a045a318954974bb6b109360d5157aa333759b7
hifistar <yenshan@gk9.so-net.ne.jp>
2019-09-21 19:23:29 +0900

    fix buf

--- 99questions/p26.hs
+++ 99questions/p26.hs
@@ -1,7 +1,24 @@
 import Data.List
 
-
+{-
+    - NG answer
 combinations :: Eq a => Int -> [a] -> [[a]]
 combinations 0 _ = [[]]
 combinations n xs = [a : b | a <- xs, b <- combinations (n-1) (delete a xs)]
+-}
+
+subset :: [a] -> [[a]]
+subset [] = [[]]
+subset (x:xs) = [ x : a | a <- subset xs] ++ subset xs
+
+combinations :: Int -> [a] -> [[a]]
+combinations n xs = [ a | a <- subset xs, length a == n]
+
+
+
+combinations' :: Int -> [a] -> [[a]]
+combinations' 1 xs = [[a] | a <- xs]
+combinations' n xs | n == length xs = [xs]
+combinations' n (x:xs) = [x : a | a <- combinations' (n-1) xs] ++ (combinations' n xs)
+
 
