088aeea77d18140f4b267f953a555a69491aa56f
hifistar <yenshan@gk9.so-net.ne.jp>
2019-11-04 09:31:38 +0900

    add file

--- 99questions/p31.hs
+++ 99questions/p31.hs
@@ -4,3 +4,4 @@ isPrime :: Int -> Bool
 isPrime n = length (divisable n) == 0
             where
                 divisable x = [a | a <- [2..x-1], x `mod` a == 0]
+
---
+++ 99questions/p46.hs
@@ -0,0 +1,35 @@
+
+
+
+and' True True = True
+and' False _ = False
+and' _ False = False
+
+or' True _ = True
+or' _ True = True
+or' False False = False
+
+nand' a b = not (and' a b)
+
+nor' a b = not (or' a b)
+
+xor' True True = False
+xor' False False = False
+xor' _ _ = True
+
+
+impl' True True = True
+impl' True False = False
+impl' False True = True
+impl' False False = True
+
+eqal' True True = True
+eqal' False False = True
+eqal' _ _ = False
+
+table f = do
+            putStrLn $ "True True " ++ show (f True True)
+            putStrLn $ "True False " ++ show (f True False)
+            putStrLn $ "False True " ++ show (f False True)
+            putStrLn $ "False False " ++ show (f False False)  
+            return ()
