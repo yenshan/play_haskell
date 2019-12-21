38d1d99bfcbf4a214ab5bf6c346f5b62c4f9e190
hifistar <yenshan@gk9.so-net.ne.jp>
2019-12-21 21:58:49 +0900

    add file

---
+++ 99questions/p59.hs
@@ -0,0 +1,16 @@
+import Data.List
+
+data Tree a = Empty | Branch a (Tree a) (Tree a) deriving (Show,Eq)
+
+leaf :: a -> Tree a
+leaf x = Branch x Empty Empty
+
+    
+hBalTree x 1 = [Empty, leaf x]
+hBalTree x n = [Branch x a b | a <- hBalTree x (n-1), b <- hBalTree x (n-1), a /= Empty || b /= Empty]
+
+main = do
+         print $ take 4 $ hBalTree 'x' 3
+         return ()
+
+         
