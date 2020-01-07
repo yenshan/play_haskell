98c9337ab5c728e272ba5a846787e92b21fdef7f
hifistar <yenshan@gk9.so-net.ne.jp>
2020-01-07 16:43:26 +0900

    modified code.

--- 99questions/p62b.hs
+++ 99questions/p62b.hs
@@ -1,15 +1,15 @@
 
 data Tree a = Empty | Branch a (Tree a) (Tree a) deriving (Show,Eq)
 
-internals Empty = []
-internals (Branch a Empty Empty) = []
-internals (Branch a l r) = [a] ++ internals l ++ internals r
+atLevel Empty _ = []    
+atLevel (Branch a _ _) 1 = [a]
+atLevel (Branch _ l r) n = atLevel l (n-1) ++ atLevel r (n-1)
 
 tree4 = Branch 1 (Branch 2 Empty (Branch 4 Empty Empty))
                  (Branch 2 Empty Empty)
 
 main = do
-         print $ internals tree4 
+         print $ atLevel tree4 2
          return ()
 
          
